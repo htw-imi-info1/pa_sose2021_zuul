a827685ec98ae5f9970c8552e6b90feaa6131ff8
Barne Kleinen <barne.kleinen@htw-berlin.de>
2021-07-18 04:08:43 +0200

    solution pa2

--- GameStatus.java
+++ GameStatus.java
@@ -3,25 +3,55 @@
  * uncomment the implements part if you want the compiler to help
  * with the correct method signatures.
  */
+import java.util.*;
 public class GameStatus
 {
     private Room currentRoom;
+    private Room lastRoom;
+    private Room beamer;
+    private Set<Room> visited;
+    private int numberOfRooms;
+    private boolean seenAll = false;
 
     public GameStatus(Room initialRoom){
-        this.currentRoom = initialRoom;
+        setLocation(initialRoom);
     }
+
     public void setLocation(Room room){
+        lastRoom = currentRoom;
         currentRoom = room;
     }
+
     public Room getLocation(){
         return currentRoom;
     }
+
+    public boolean goBack(){
+        if (lastRoom == null)
+            return false;
+        currentRoom = lastRoom;
+        lastRoom = null;
+        return true;
+    }
+
+    public void chargeBeamer(){
+        beamer = currentRoom;
+    }
+
+    public boolean fireBeamer(){
+        if (beamer == null) return false;
+        setLocation(beamer);
+        beamer = null;
+        return false;
+    }
+
     public String getLocationDescription(){
         return "You are " +currentRoom.getDescription();
     }
 
     private boolean playing = true;
     public void quit(){playing = false;}
+
     public boolean isPlaying(){return playing;}
 
 }
